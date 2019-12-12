7dc35f44712f9ccef78d6ff5113cd4ad3988e3bb
Mahendra Prajapati <mahendra.prajapati@comviva.com>
2019-12-12 14:23:52 +0530

    fromIterable and ConnectableObservable added

---
+++ introduction/src/com/maso/reactive/factory/cold/FromIteratorFactory.java
@@ -0,0 +1,25 @@
+package com.maso.reactive.factory.cold;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.reactivex.Observable;
+
+public class FromIteratorFactory {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Creation of the cold Observable using fromIterable() factory method of
+		 * Observable Interface
+		 */
+
+		List<String> emissions = Arrays.asList("First", "Second", "Third", "Fourth");
+		Observable<String> source = Observable.fromIterable(emissions);
+
+		source.subscribe(e -> System.out.println("Observer 1 : " + e));
+		source.subscribe(e -> System.out.println("Observer 2 : " + e));
+
+	}
+
+}
